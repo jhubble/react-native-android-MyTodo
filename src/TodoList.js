cffcf35578dddf9b892224ecee385481b09befa3
Jeremy Hubble <jhubble@hollandamericagroup.com>
2017-06-09 17:22:49 -0700

    Reset tasks after 3am.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
-import {Text, View, ListView, StyleSheet, AsyncStorage} from 'react-native';
+import {AppState, Text, View, ListView, StyleSheet, AsyncStorage} from 'react-native';
 import TodoModel from './TodoModel';
 import OmniBox from './OmniBox';
 import ListViewItem from './ListViewItem';
 
-let dataList = [
-];
+let dataList = [];
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -23,6 +22,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#ff0000'
     }
 });
+const oneDay = 1000*60*60*24;
+const resetTime = 3;
+
 
 class TodoList extends Component {
     constructor(props) {
@@ -30,6 +32,7 @@ class TodoList extends Component {
         this.updateDataList = this.updateDataList.bind(this);
         this.markDone = this.markDone.bind(this);
         this.dumpData = this.dumpData.bind(this);
+        this.loadData = this.loadData.bind(this);
 
         const ds = new ListView.DataSource({
             rowHasChanged: (r1, r2) => {
@@ -40,26 +43,84 @@ class TodoList extends Component {
         this.state = {
             dataList: dataList,
             dataSource: ds.cloneWithRows(dataList)
-        }
+        };
+
+        this.loadData();
+
+        // This is not the way to do it!
+        setInterval(() => {
+            this.timeToReset();
+        },10*60*1000);
     }
 
     markDone(item) {
         this.dumpData();
     }
 
+    resetTasks() {
+        this.setState((prevState, props) => {
+            if (prevState.dataList) {
+                prevState.dataList.map((item) => {
+                    item.completed = false;
+                    return item;
+                });
+                prevState.lastResetTime = Date.now();
+                AsyncStorage.setItem('@myTodo:lastReset',prevState.lastResetTime)
+            }
+            return prevState;
+        })
+    }
+
+    timeToReset() {
+        // Reset is past 3am or 24 hours past last reset
+        // Date.time
+        var now = new Date();
+        var then = this.state.lastResetTime;
+        var milidiff = now-then;
+        if ((now.getHours() >resetTime && now.getDate() != then.getDate()) ||(milidiff > oneDay)) {
+            this.resetTasks();
+        }
+    }
+
+
+    loadData() {
+        AsyncStorage.getItem('@MyTodo:lastReset', (err, result) => {
+            if (result) {
+                this.setState((prevState, props) => {
+                    prevState.lastResetTime = result - 0;
+                    setImmediate(() => {
+                        this.timeToReset();
+                    });
+                    return prevState;
+                })
+            }
+        });
+
+        AsyncStorage.getItem('@MyTodo:todoitems', (err, result) => {
+            console.log("Loaded data:", result);
+            if (result) {
+                let dataList = JSON.parse(result);
+                this.setState((prevState, props) => {
+                    prevState.dataList = dataList;
+                    prevState.dataSource = prevState.dataSource.cloneWithRows(dataList);
+                    return prevState;
+                });
+            }
+        })
+    }
+
     dumpData() {
         var dataObj = JSON.stringify(this.state.dataList);
-        console.log("SAVING dataObj:",dataObj);
+        console.log("SAVING dataObj:", dataObj);
         AsyncStorage.setItem('@MyTodo:todoitems', dataObj);
     }
 
     updateDataList(dataItem) {
         this.setState((prevState, props) => {
             let dataList = prevState.dataList.concat([new TodoModel(dataItem, false)]);
-            return {
-                dataList: dataList,
-                dataSource: prevState.dataSource.cloneWithRows(dataList)
-            }
+            prevState.dataList = dataList;
+            prevState.dataSource = prevState.dataSource.cloneWithRows(dataList)
+            return prevState;
         });
         this.dumpData();
     }
@@ -73,7 +134,8 @@ class TodoList extends Component {
                     dataSource={this.state.dataSource}
                     style={styles.container}
                     renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator}/> }
-                    renderRow={(data) => <ListViewItem dumpData={this.dumpData} markDone={this.markDone} data={data}/>}
+                    renderRow={(data) => <ListViewItem dumpData={this.dumpData} markDone={this.markDone}
+                                                       data={data}/>}
                 />
             );
         }
